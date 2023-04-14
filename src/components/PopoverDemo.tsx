bf8e1f8e370afeecd4b49d94fa97a05a7e8c1840
Mehmet <mehmetext68@gmail.com>
2023-04-14 06:16:21 +0300

    added Popover.Close

--- src/components/PopoverDemo.tsx
+++ src/components/PopoverDemo.tsx
@@ -1,5 +1,4 @@
 import * as Popover from "@radix-ui/react-popover";
-import { PopoverArrow } from "@radix-ui/react-popover";
 
 export default function PopoverDemo() {
 	return (
@@ -14,10 +13,15 @@ export default function PopoverDemo() {
 					sideOffset={4}
 					className="bg-blue-500 p-2 rounded text-white flex flex-col gap-2"
 				>
+					<div className="flex justify-end">
+						<Popover.Close className="bg-blue-400 -mt-5 -mr-5 w-8 h-8 rounded-full">
+							x
+						</Popover.Close>
+					</div>
 					<div className="px-8 py-2 bg-blue-600 rounded">Deneme</div>
 					<div className="px-8 py-2 bg-blue-600 rounded">Deneme</div>
 					<div className="px-8 py-2 bg-blue-600 rounded">Deneme</div>
-					<PopoverArrow className="fill-blue-500" />
+					<Popover.Arrow className="fill-blue-500" />
 				</Popover.Content>
 			</Popover.Portal>
 		</Popover.Root>
